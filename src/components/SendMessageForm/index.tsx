fe82635750c8d08ab7fa1d48a4beb1b5c43a7a64
Ariane Brandão <ariane_mad@hotmail.com>
2022-02-11 17:26:05 -0300

    update styles

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,57 +1,103 @@
-import { useContext, useState, FormEvent } from 'react'
-import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
-import { AuthContext } from '../../contexts/auth'
-import { api } from '../../services/api'
-import styles from './styles.module.scss'
+import { useContext, useState, FormEvent } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { VscGithubInverted, VscSignOut } from "react-icons/vsc";
+import { AuthContext } from "../../contexts/auth";
+import { api } from "../../services/api";
+import styles from "./styles.module.scss";
 
 export function SendMessageForm() {
-  
-  const { user, signOut } = useContext(AuthContext)
-  const [message, setMessage] = useState('')
+  const { user, signOut } = useContext(AuthContext);
+  const [message, setMessage] = useState("");
+  const [sendingMessage, setSendingMessage] = useState(false);
 
   async function handleSendMessage(event: FormEvent) {
     event.preventDefault();
 
     if (!message.trim()) {
       return;
+    } else {
+      try {
+        setSendingMessage(true);
+
+        await api.post("messages", { message });
+
+        setMessage(""); // limpa form
+        toast.success("Mensagem enviada com sucesso!", {
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+
+        setSendingMessage(false);
+      } catch (err) {
+        if (err instanceof Error) {
+          toast.error("Oops... algo deu errado. Por favor, tente novamente.", {
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+            },
+          });
+          console.log(err);
+        } else {
+          toast.error("Oops... algo deu errado. Por favor, tente novamente.", {
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+            },
+          });
+          console.log(err);
+        }
+      }
     }
-
-    await api.post('messages', { message })
-    setMessage('') // limpa form
-
   }
 
   return (
-    <div className={styles.sendMessageFormWrapper}>
-      <button onClick={signOut} className={styles.signOutButton}>
-        <VscSignOut size="32" />
-      </button>
+    <>
+      <div>
+        <Toaster />
+      </div>
 
-      <header className={styles.userInformation}>
-        <div className={styles.userImage}>
-          <img src={user?.avatar_url} alt={user?.name} />
-        </div>
+      <div className={styles.sendMessageFormWrapper}>
+        <button onClick={signOut} className={styles.signOutButton}>
+          <VscSignOut size="32" />
+        </button>
 
-        <strong className={styles.userName}>{user?.name}</strong>
-        <span className={styles.userGithub}>
-          <VscGithubInverted size="16" />
-          {user?.login}
-        </span>
-      </header>
+        <header className={styles.userInformation}>
+          <div className={styles.userImage}>
+            <img src={user?.avatar_url} alt={user?.name} />
+          </div>
 
-      <form onSubmit={ handleSendMessage } className={styles.sendMessageForm}>
-        <label htmlFor="message">Mensagem</label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder="Qual sua expectativa para o evento?"
-          onChange={event => setMessage(event.target.value)}
-          value={message}
-          maxLength={140}
-        />
+          <strong className={styles.userName}>{user?.name}</strong>
+          <span className={styles.userGithub}>
+            <VscGithubInverted size="16" />
+            {user?.login}
+          </span>
+        </header>
 
-        <button type="submit">Enviar mensagem</button>
-      </form>
-    </div>
-  )
+        <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
+          <label htmlFor="message">Mensagem</label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Qual sua expectativa para o evento?"
+            onChange={(event) => setMessage(event.target.value)}
+            value={message}
+            maxLength={140}
+          />
+
+          {sendingMessage ? (
+            <button type="submit">Enviar mensagem</button>
+          ) : (
+            <button type="button" disabled>
+              Enviando mensagem
+            </button>
+          )}
+        </form>
+      </div>
+    </>
+  );
 }
